e283fdbb374b7ca1b5d62bb5684f970dd05ef378
ggabmartins <gabriellm1805@gmail.com>
2025-05-28 20:10:15 -0300

    Classe Informacao e BoasPraticas finalizadas.

--- src/br/com/fiap/bean/BoasPraticas.java
+++ src/br/com/fiap/bean/BoasPraticas.java
@@ -1,4 +1,12 @@
 package br.com.fiap.bean;
 
-public class BoasPraticas {
+public class BoasPraticas extends Informacao {
+    public BoasPraticas() {
+        super("Boas práticas para prevenção e segurança em enchentes:\n"
+                + "- Evite transitar por áreas alagadas.\n"
+                + "- Mantenha objetos de valor em locais altos.\n"
+                + "- Acompanhe alertas da Defesa Civil.\n"
+                + "- Tenha um plano de emergência familiar.\n"
+                + "- Descarte corretamente o lixo para evitar entupimentos.\n");
+    }
 }
--- src/br/com/fiap/bean/Informacao.java
+++ src/br/com/fiap/bean/Informacao.java
@@ -1,4 +1,20 @@
 package br.com.fiap.bean;
 
 public class Informacao {
+    private String Texto;
+
+    //Construtor
+    public Informacao(String Texto) {
+        this.Texto = Texto;
+    }
+
+    //Getters e setters
+
+    public String getTexto() {
+        return Texto;
+    }
+
+    public void setTexto(String texto) {
+        Texto = texto;
+    }
 }
