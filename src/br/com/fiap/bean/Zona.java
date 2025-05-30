91bd82fb7c745fd05a0e222401571a43d4b86543
ggabmartins <gabriellm1805@gmail.com>
2025-05-30 05:00:04 -0300

    Metodo adicionado na classe Zona.

--- src/br/com/fiap/bean/Zona.java
+++ src/br/com/fiap/bean/Zona.java
@@ -20,7 +20,33 @@ public class Zona {
     public String getBairrosPerigosos() {
         return bairrosPerigosos;
     }
-    public void setBairrosPerigosos(String bairrosPerigosos) {
+    public void setBairrosPerigosos(String bairrosPerigosos){
+    }
 
+    //metodos particulares
+    public static String getZonas() {
+        // Retorna as zonas separadas por vírgula ou número para o menu
+        return "1 - Zona Norte\n"
+                + "2 - Zona Sul\n"
+                + "3 - Zona Leste\n"
+                + "4 - Zona Oeste\n"
+                + "5 - Centro\n";
+    }
+
+    public static String getBairrosPorZona(int opcaoZona) {
+        switch(opcaoZona) {
+            case 1:
+                return "Jaçanã, Tremembé, Vila Maria";
+            case 2:
+                return "Capão Redondo, Grajaú, Jardim Ângela";
+            case 3:
+                return "São Mateus, Itaquera, Guaianases";
+            case 4:
+                return "Butantã, Pinheiros, Lapa";
+            case 5:
+                return "Sé, República, Bela Vista";
+            default:
+                return "Zona inválida";
+        }
     }
 }
